26fe9f40781d9fc5f2274204a6c1ddd49d0049c5
Jack Belford <jackbelford12@live.ca>
2019-01-21 00:03:16 -0700

    parsing win conditions from mods

--- src/main/soulstorm.ts
+++ src/main/soulstorm.ts
@@ -1,57 +1,111 @@
 import * as fs from 'fs';
 import * as ini from 'ini';
+import * as lua from 'luaparse';
 import * as path from 'path';
+import { AppData } from './appdata';
 
 export namespace Soulstorm {
 
-  export function getModules(dir: string): Module[] {
-    const modules = fs.readdirSync(dir, { withFileTypes: true })
-      .filter(file => file.isFile() && /\.module$/.test(file.name))
-      .map(file => path.join(dir, file.name))
-      .map(filepath => fs.readFileSync(filepath, 'utf8'))
-      .map(data => ini.parse(data).global)
-      .map(config => (<Module>{
-        name: config.UIName,
-        description: config.Description,
-        dllName: config.DllName,
-        modFolder: config.ModFolder,
-        version: config.ModVersion,
-        textureFe: config.TextureFE,
-        dataFolders: Object.keys(config).filter(key => /^DataFolder\.\d+$/.test(key)).map(key => config[key]),
-        requiredMods: Object.keys(config).filter(key => /^RequiredMod\.\d+$/.test(key)).map(key => config[key])
-      }))
-      .map(mod => replaceLocales(dir, mod));
+  let localeData: { [key: string]: string };
+  let mods: Module[];
+  let winConditions: WinCondition[];
 
+  export function getModules(): Module[] {
+    if (!mods) {
+      const { dir } = AppData.getSettings();
+      mods = fs.readdirSync(dir, { withFileTypes: true })
+        .filter(file => file.isFile() && /\.module$/.test(file.name))
+        .map(file => path.join(dir, file.name))
+        .map(filepath => fs.readFileSync(filepath, 'utf8'))
+        .map(data => ini.parse(data).global)
+        .map(config => ({
+          name: config.UIName,
+          description: config.Description,
+          dllName: config.DllName,
+          modFolder: path.join(dir, config.ModFolder),
+          version: config.ModVersion,
+          textureFe: config.TextureFE,
+          dataFolders: Object.keys(config).filter(key => /^DataFolder\.\d+$/.test(key)).map(key => config[key]),
+          requiredMods: Object.keys(config).filter(key => /^RequiredMod\.\d+$/.test(key)).map(key => config[key])
+        }));
 
-    return modules;
+      // A bit of goofy recursion happens here but whatever
+      mods = replaceLocales(mods);
+    }
+    return mods;
   }
 
-  function replaceLocales(dir: string, mod: Module) {
-    const keys = Object.keys(mod)
-      .filter(key => typeof mod[key] === 'string')
-      .filter(key => /^\$\d+$/.test(mod[key]));
+  export function getModWinConditions(): WinCondition[] {
+    if (!winConditions) {
+      winConditions = getModules().map((mod, i) => {
+        const winConditionsPath = path.join(mod.modFolder, 'Data', 'scar', 'winconditions');
+        try {
+          return fs.readdirSync(winConditionsPath, { withFileTypes: true })
+            .filter(file => file.isFile() && /\.lua$/.test(file.name))
+            .map(file => path.join(winConditionsPath, file.name))
+            .map(filepath => lua.parse(fs.readFileSync(filepath, 'utf8')))
+            .map((data: any) => {
+              const body = data.body.find((body: any) => body.type === 'AssignmentStatement'
+                && body.variables.every((v: any) => v.type === 'Identifier' && v.name === 'Localization'));
+              if (!body) return null;
+
+              const init = body.init.find((init: any) => init.type === 'TableConstructorExpression');
+              if (!init) return null;
+
+              const values = {};
+              init.fields.forEach((field: any) => values[field.key.name] = field.value.value);
+
+              return replaceLocales(<WinCondition>{
+                mod: i,
+                title: values['title'],
+                description: values['description'],
+                victoryCondition: values['victory_condition'],
+                alwaysOn: values['always_on'],
+                exclusive: values['exclusive'],
+              });
+            })
+            .filter(wc => !!wc && !!wc.title && !!wc.title.trim()) as WinCondition[];
+        } catch (e) {
+          return [];
+        }
+      })
+        .reduce((prev, curr) => prev.concat(curr))
+        .sort((a, b) => a.mod === b.mod ? a.title.localeCompare(b.title) : a.mod - b.mod);
+
+    }
+    return winConditions;
+  }
+
+  function replaceLocales<T>(obj: T): T {
+    const keys = Object.keys(obj)
+      .filter(key => typeof obj[key] === 'string')
+      .filter(key => /^\$\d+$/.test(obj[key]));
 
     if (keys.length > 0) {
-      const localeData = getLocaleData(dir, mod);
+      const localeData = getLocaleData();
       keys.forEach(key => {
-        mod[key] = localeData[mod[key]];
+        obj[key] = localeData[obj[key]];
       });
     }
 
-    return mod;
+    return obj;
   }
 
-  function getLocaleData(dir: string, mod: Module) {
-    const localeData = {};
-    const localePath = path.join(dir, mod.modFolder, 'locale', 'English');
-    fs.readdirSync(localePath, { withFileTypes: true })
-      .filter(file => file.isFile() && /\.ucs$/.test(file.name))
-      .map(file => fs.readFileSync(path.join(localePath, file.name), { encoding: 'ucs2' }))
-      .forEach(data => {
-        data.trim().split(/\n+/g).map(line => line.split(/\s+/g)).forEach(parts => {
-          localeData['$' + parts[0].trim()] = parts.slice(1).join(' ');
-        });
+  function getLocaleData() {
+    if (!localeData) {
+      localeData = {};
+      getModules().forEach(mod => {
+        const localePath = path.join(mod.modFolder, 'Locale', 'English');
+        fs.readdirSync(localePath, { withFileTypes: true })
+          .filter(file => file.isFile() && /\.ucs$/.test(file.name))
+          .map(file => fs.readFileSync(path.join(localePath, file.name), { encoding: 'ucs2' }))
+          .forEach(data => {
+            data.trim().split(/\n+/g).map(line => line.split(/\s+/g)).forEach(parts => {
+              localeData['$' + parts[0].trim()] = parts.slice(1).join(' ');
+            });
+          });
       });
+    }
     return localeData;
   }
 
@@ -64,6 +118,15 @@ export namespace Soulstorm {
     textureFe: string;
     dataFolders: string[];
     requiredMods: string[];
+  };
+
+  export type WinCondition = {
+    mod: number;
+    title: string;
+    description: string;
+    victoryCondition: boolean;
+    exclusive: boolean;
+    alwaysOn: boolean;
   };
 
 }
